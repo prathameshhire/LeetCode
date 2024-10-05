d760f391dd9ba7ba5f225dd38e4ee60e0bc979a2
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2024-10-05 17:43:07 -0400

    Time: 6 ms (68.06%), Space: 8.8 MB (80.32%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n = s1.size(); 
-        int m = s2.size();
+        int n=s1.size();
+        int m=s2.size();
 
-        if (n>m) return false;
+        if(n>m) return false;
 
-        sort(s1.begin(),s1.end());
+        vector<int> s1_freq(26,0);
+        vector<int> s2_freq(26,0);
 
-        for (int i=0; i<=m-n; i++) {
-            string substring = s2.substr(i,n);
-            sort(substring.begin(), substring.end());
+        int i=0;
+        int j=0;
 
-            if(s1==substring) return true;
+        for (char &ch:s1) {
+            s1_freq[ch-'a']++;
+        }
+
+        while (j<m) {
+            s2_freq[s2[j]-'a']++;
+
+            if (j-i+1>n) {
+                s2_freq[s2[i]-'a']--;
+                i++;
+            }
+
+            if (s1_freq == s2_freq) {
+                return true;
+            }
+            j++;
         }
         return false;
     }
