e2e172e983a5ed0215a80906eb63eb920ab7c0db
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2024-11-09 15:28:08 -0500

    Time: 6 ms (15.00%), Space: 11.8 MB (8.38%) - LeetHub

--- 0369-plus-one-linked-list/0369-plus-one-linked-list.cpp
+++ 0369-plus-one-linked-list/0369-plus-one-linked-list.cpp
@@ -10,36 +10,23 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
-        if (head == NULL || head->next == NULL) return head;
-        ListNode* newHead = reverse(head->next);
-        ListNode* front = head->next;
-        front->next = head;
-        head->next = NULL;
-        return newHead;
+    int helper(ListNode* temp) {
+        if (!temp) return 1;
+        int carry = helper(temp->next);
+        temp->val = temp->val + carry;
+        if (temp->val < 10) {
+            return 0;
+        }
+        temp->val = 0;
+        return 1;
     }
 
     ListNode* plusOne(ListNode* head) {
-        head = reverse(head);
-        ListNode* temp = head;
-        int carry = 1;
-        while (temp) {
-            temp->val = temp->val + carry;
-            if (temp->val < 10) {
-                carry = 0;
-                break;
-            } else {
-                carry = 1;
-                temp->val = 0;
-            }
-            temp = temp->next;
-        }
-        if (carry == 1) {
-            head = reverse(head);
-            ListNode* newHead = new ListNode(1, head);
+        int carry = helper(head);
+        if (carry) {
+            ListNode* newHead = new ListNode (1,head);
             return newHead;
         }
-        head = reverse(head);
         return head;
     }
 };
