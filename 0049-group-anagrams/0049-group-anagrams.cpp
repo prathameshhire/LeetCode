7ae0391ad16d35c2aa2e13458639602bffaab2c8
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-02-19 02:57:40 -0500

    Time: 75 ms (8.38%), Space: 37.3 MB (5.51%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map <string, vector<string>> mp;
+        for (auto s:strs) {
+            vector<int> count(26,0);
+            for (auto c:s) {
+                count[c-'a']++;
+            }
+            string key = to_string(count[0]);
+            for (int i=1; i<26; i++) {
+                key += ',' + to_string(count[i]);
+            }
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (auto p: mp) {
+            result.push_back(p.second);
+        }
+        return result;
+    }
+};
