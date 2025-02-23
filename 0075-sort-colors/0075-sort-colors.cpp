14389cadaec55f89b3cf2498922d4d32c4f7c6ba
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-02-22 23:38:07 -0500

    Time: 0 ms (100%), Space: 11.6 MB (32.12%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
-    }
-
-    void mergeSort(vector<int>& nums, int low, int high) {
-        if (low>=high) return;
-        int mid = (low+high)/2;
-        mergeSort(nums, low, mid);
-        mergeSort(nums, mid+1, high);
-        merge(nums, low, mid, high);
-    }
-
-    void merge(vector<int>& nums, int low, int mid, int high) {
-        vector<int> temp;
-        int left = low;
-        int right = mid+1;
-        while (left<=mid && right<=high) {
-            if (nums[left]<=nums[right]) {
-                temp.push_back(nums[left++]);
-            }
-            else {
-                temp.push_back(nums[right++]);
-            }
+        vector<int> bucket(3,0);
+        for (int num:nums) {
+            bucket[num]++;
         }
-        while (left<=mid) temp.push_back(nums[left++]);
-        while (right<=high) temp.push_back(nums[right++]);
-        for (int i=low; i<=high; i++) {
-            nums[i] = temp[i-low]; 
+        int index = 0;
+        for (int i=0; i<3; i++) {
+            while (bucket[i]-->0) {
+                nums[index++] =i;
+            }
         }
     }
 };
