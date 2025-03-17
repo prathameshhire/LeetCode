266c57eb8cf406d1331baf094ac6ffba53cd2d10
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-03-17 03:37:19 -0400

    Time: 2 ms (19.43%), Space: 20.3 MB (8.9%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map <int,int> count;
-        for (auto it: nums) {
+        unordered_map <int, int> count;
+        for (auto it :nums) {
             count[it]++;
-            if (count.size() > 2) {
-                unordered_map <int,int> newcount;                
-                for (auto entry: count) {
-                    newcount[entry.first] = entry.second-1; 
+            
+            if (count.size()>2) {
+                unordered_map <int, int> newCount;
+                for (auto entry : count) {
+                    if (entry.second > 1) {
+                        newCount[entry.first] = entry.second - 1;
+                    }
                 }
-                count = newcount;
-
+                count = newCount;
             }
         }
 
         vector <int> ans;
-        for (auto entry:count) {
+        for (auto entry : count) {
             int frequency = 0;
-            for (auto num:nums) {
-                if (num == entry.first) {
+            for (int num:nums) {
+                if(num == entry.first) {
                     frequency++;
                 }
             }
