5e7a0fa7bd43ef0761185b0ad782442c987e3e15
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-03-18 01:44:47 -0400

    Time: 3 ms (32.52%), Space: 10.7 MB (6.29%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,54 +1,12 @@
 class Solution {
-private:
-bool valid (char ch) {
-    if ((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9')){
-        return 1;
-    }
-    return 0;
-}
-char lowerCase(char ch){
-    if ((ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
-        return ch;
-    }
-    else {
-        char temp;
-        temp = ch - 'A' +'a';
-        return temp;
-    }
-}
-
-bool palindrome(string a){
-    int i=0;
-    int k=a.length()-1;
-
-    while (i<=k){
-        if (a[i]!=a[k]){
-            return 0;
-        }
-        else {
-            i++;
-            k--;
-        }
-    }
-    return 1;
-
-}
-
 public:
     bool isPalindrome(string s) {
-        int i=0;
-        string temp ="";
-        
-        for (int j=0; j <s.length(); j++){
-            if (valid(s[j])){
-                temp.push_back(s[j]);
+        string newStr = "";
+        for (char c:s) {
+            if (isalnum(c)) {
+                newStr+=tolower(c);
             }
         }
-
-        for (int j=0; j<temp.length(); j++){
-            temp[j]=lowerCase(temp[j]);
-        }
-
-        return palindrome(temp);
+        return newStr == string(newStr.rbegin(), newStr.rend());
     }
 };
