c29135fac70b13c211b824f282c42d3c4196edaa
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-05-08 02:47:50 -0400

    Time: 0 ms (100%), Space: 8.5 MB (50.22%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        vector<int> freq(26, 0);
+        for (char& c : s) freq[c-'a']++;
+
+        priority_queue<pair<int, char>> maxHeap;
+        for (int i = 0; i<26; i++) {
+            if (freq[i] > 0) maxHeap.push({freq[i], 'a'+i});
+        }
+
+        string res = "";
+        pair<int, char> prev = {0, ' '};
+        while (!maxHeap.empty() || prev.first > 0) {
+            if (prev.first > 0 && maxHeap.empty()) return "";
+            auto[count,character] = maxHeap.top();
+            maxHeap.pop();
+            res += character;
+            count--;
+
+            if (prev.first > 0) maxHeap.push(prev);
+            prev = {count, character};
+        }
+        return res;
+    }
+};
