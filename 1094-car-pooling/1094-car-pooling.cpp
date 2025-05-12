785d786d9bbbe2f7b64025f67b63ac3a4db9d72b
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-05-12 02:15:03 -0400

    Time: 0 ms (100%), Space: 14.2 MB (82.73%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin(), trips.end(), [] (const vector<int> &a, const vector<int> &b) {
-            return a[1] < b[1];
-        });
-
-        int currPass = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
-        for (auto &trip : trips) {
+        vector<int> delta (1001, 0);
+        for (auto &trip:trips) {
             int numPass = trip[0], start = trip[1], end = trip[2];
-            while (!minHeap.empty() && minHeap.top().first <= start) {
-                currPass -= minHeap.top().second;
-                minHeap.pop();
-            }
-
-            currPass += numPass;
-            if (currPass > capacity) return false;
-            minHeap.emplace(end,numPass);
+            delta[start] += numPass;
+            delta[end] -= numPass;
+        }
+        int onBoard = 0;
+        for (int &s : delta) {
+            onBoard += s;
+            if (onBoard>capacity) return false;
         }
         return true;
     }
