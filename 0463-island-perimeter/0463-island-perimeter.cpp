a4a77ef4143a649a62709c6a47cb00711105cc67
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-06-02 22:34:19 -0400

    Time: 98 ms (5.04%), Space: 107.9 MB (10.08%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int res = 0;
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (grid[r][c]) {
-                    res += 4;
-                    if (r && grid[r - 1][c]) {
-                        res -= 2;
+        int rows = grid.size(), cols = grid[0].size();
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        int directions[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == 1) {
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    visited[i][j] = true;
+                    int perimeter = 0;
+
+                    while (!q.empty()) {
+                        auto [x, y] = q.front();
+                        q.pop();
+
+                        for (auto& dir : directions) {
+                            int nx = x + dir[0], ny = y + dir[1];
+                            if (nx < 0 || ny < 0 || nx >= rows || 
+                                ny >= cols || grid[nx][ny] == 0) {
+                                perimeter++;
+                            } else if (!visited[nx][ny]) {
+                                visited[nx][ny] = true;
+                                q.push({nx, ny});
+                            }
+                        }
                     }
-                    if (c && grid[r][c - 1]) {
-                        res -= 2;
-                    }
+                    return perimeter;
                 }
             }
         }
-        return res;
+        return 0;
     }
 };
