391e7d81091572970c8e07250c07c51bfb3dd189
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-06-03 22:05:17 -0400

    Time: 75 ms (41.05%), Space: 169.2 MB (24.63%) - LeetHub

--- 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3403-find-the-lexicographically-largest-string-from-the-box-i/3403-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -4,9 +4,7 @@ public:
         if (numFriends==1) return word;
         int n = word.size(), window = n-numFriends+1;
         string result = "";
-        for (int i = 0; i<n; i++) {
-            result = max(result, word.substr(i,window));
-        }
+        for (int i = 0; i<n; i++) result = max(result, word.substr(i,window));
         return result;
     }
 };
