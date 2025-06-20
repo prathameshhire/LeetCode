a7f162ab877cf3fa41fbe57ae53d3d410ac1edd9
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-06-19 23:08:39 -0400

    Time: 0 ms (100%), Space: 74.9 MB (87.85%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
-    bool isValid(vector<int> &nums, int mid, int n) {
-        vector<long long> arr(nums.begin(), nums.end());
-        long long buffer = 0;
-        for (int i = 0; i<n-1; i++) {
-            if (arr[i] > mid) return false;
-            buffer = mid - arr[i];
-            arr[i+1] -= buffer;
-        }
-        return arr[n-1] <= mid;
-    }
     int minimizeArrayValue(vector<int>& nums) {
-        int result = 0;
-        int left = 0;
-        int right = *max_element(nums.begin(), nums.end());
-        int n = nums.size();
-        while (left<=right) {
-            int mid = left + (right-left)/2;
-            if (isValid(nums, mid, n)) {
-                result = mid;
-                right = mid-1;
-            }
-            else left = mid+1;
-        } 
-        return result;
+        long long ans = nums[0];    
+        long long total = nums[0];    
+        for(int i = 1; i < nums.size(); i++){
+            total += nums[i];
+            ans = max(ans, (total + i)/(i+1));
+        }
+        return (int)ans;
     }
 };
