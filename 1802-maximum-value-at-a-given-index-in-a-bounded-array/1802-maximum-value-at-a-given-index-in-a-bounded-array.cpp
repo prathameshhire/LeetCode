72869f043e72a15f6d295d38a5d4e911bec7d47a
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-07-02 23:37:48 -0400

    Time: 0 ms (100%), Space: 7.9 MB (96.12%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     typedef long long ll;
-    ll getSum(ll count, ll x) {
-        return (count*x) - (count*(count+1))/2;
+    ll formula (ll count, ll x) {
+        return (count*x) - (count*(count+1))/2; 
     }
-
     int maxValue(int n, int index, int maxSum) {
         ll left = 1;
+        ll mid_val;
         ll right = maxSum;
-        ll mid;
         int result = 0;
         while (left<=right) {
-            mid = left + (right-left)/2;
-            ll left_count = min((ll)index, mid-1);
-            ll left_sum = getSum(left_count, mid);
-            left_sum += max((ll)0, index - (mid-1));
-            ll right_count = min((ll)n-index-1, mid-1);
-            ll right_sum = getSum(right_count, mid);
-            right_sum += max((ll)0, n-index-1 - (mid-1));
-            ll total_sum = left_sum + mid + right_sum;
+            mid_val = left + (right-left)/2;
+            ll left_count = min ((ll)index, mid_val-1);
+            ll left_sum = formula(left_count, mid_val);
+            left_sum += max((ll)0, index - (mid_val-1)); 
+            ll right_count = min ((ll)n-index-1, mid_val-1);
+            ll right_sum = formula(right_count, mid_val);
+            right_sum += max((ll)0, n-index-1 - (mid_val-1)); 
+            ll total_sum = left_sum + right_sum + mid_val;
             if (total_sum <= maxSum) {
-                result = max((ll)result, mid);
-                left = mid+1;
+                result = max((ll)result, mid_val);
+                left = mid_val+1;
+            } 
+            else {
+                right = mid_val-1;
             }
-            else right = mid-1;
         }
         return result;
     }
