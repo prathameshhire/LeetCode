7cbf01a574d00c7fb16d5c00f7d587915125a1b7
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-07-10 20:48:50 -0400

    Time: 0 ms (100%), Space: 9.6 MB (15.95%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int longestSubsequence(string s, int k) {
-        int sm = 0;
-        int cnt = 0;
-        int bits = 32 - __builtin_clz(k);
-        for (int i = 0; i < s.size(); ++i) {
-            char ch = s[s.size() - 1 - i];
-            if (ch == '1') {
-                if (i < bits && sm + (1 << i) <= k) {
-                    sm += 1 << i;
-                    cnt++;
-                }
-            } else {
-                cnt++;
+        int n = s.length();
+        long long powerVal = 1;
+        int length = 0;
+        for (int i = n-1; i>=0; i--) {
+            if (s[i] == '0') {
+                length++;
+            }
+            else if(powerVal <= k) {
+                length++;
+                k-=powerVal;
+            }
+            if (powerVal <= k) {
+                powerVal <<= 1;
             }
         }
-        return cnt;
+        return length;
     }
 };
