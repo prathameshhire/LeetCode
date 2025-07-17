1c0445f3b8dd17c4ec0ffd733bdb1b7d45a3fb3f
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-07-17 01:44:17 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -3,28 +3,43 @@ public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         unordered_map<int, vector<int>> mp;
+        
         for (int i = 0; i<n; i++) {
             for (int j = 0; j<n; j++) {
-                if (isConnected[i][j]) {
+                if (isConnected[i][j] == 1) {
                     mp[i].push_back(j);
                     mp[j].push_back(i);
                 }
             }
         }
-        vector<bool> visited(n, false);
+
+        vector<bool> visited(n, 0);
         int count = 0;
+
         for (int i = 0; i<n; i++) {
             if (!visited[i]) {
+                bfs (mp, 0, visited);
                 count++;
-                dfs(mp, i, visited);
             }
         }
+
         return count;
     }
-    void dfs(unordered_map<int, vector<int>> &mp, int u, vector<bool> &visited) {
-        visited[u] = true;
-        for (int &v : mp[u]) {
-            if (!visited[v]) dfs(mp, v, visited);
+
+    void bfs (unordered_map<int, vector<int>> &mp, int u, vector<bool> &visited) {
+        queue<int> q;
+        q.push(u);
+        visited[u] = 1;
+
+        while(!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (auto v:mp[u]) {
+                if (!visited[v]) {
+                    bfs(mp, v, visited);
+                }
+            }
         }
+
     }
 };
