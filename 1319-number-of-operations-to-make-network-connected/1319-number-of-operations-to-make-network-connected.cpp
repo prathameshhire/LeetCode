785a407f19a62ad4febd04542b65ac5a31b61acc
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-07-21 20:04:41 -0400

    Time: 4 ms (83.36%), Space: 42.2 MB (85.48%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -23,19 +23,13 @@ public:
         if (connections.size() < n-1) return -1;
         rank.resize(n,1);
         parent.resize(n);
-        int extra = 0;
         for (int i = 0; i<n; i++) parent[i] = i;
-        for (auto vec : connections) {
-            int a = vec[0];
-            int b = vec[1];
-            if (find(a) == find(b)) extra++;
-            else {
-                Union(a,b);
+        for (auto &vec : connections) {
+            if (find(vec[0]) != find(vec[1])) {
+                Union(vec[0],vec[1]);
+                n--;
             }
         }
-        unordered_map<int, int> mp;
-        for (auto val : parent) mp[find(val)]++;
-        int nodes = mp.size();
-        return nodes-1;
+        return n-1;
     }
 };
