ddc685bb3a2649c3b0a7b5d7cffc721d3317a7a4
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-07-25 21:00:39 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int, vector<pair<int, int>>> mp;
-        for (int i = 0; i<times.size(); i++) {
-            mp[times[i][0]].push_back({times[i][1],times[i][2]});
+        unordered_map<int, vector<pair<int, int>>> adj;
+        for (auto &edge: times) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            adj[u].push_back({v,w});
         }
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> result(n+1,INT_MAX);
+        pq.push({0, k});
+        vector<int> result(n+1, INT_MAX);
         result[k] = 0;
-        pq.push({0,k});
-        while(!pq.empty()) {
-            auto start = pq.top();
-            int d = start.first;
-            int node = start.second;
+        while (!pq.empty()) {
+            int dist = pq.top().first;
+            int node = pq.top().second;
             pq.pop();
-            for (auto &neighbour : mp[node]) {
-                int side = neighbour.first;
-                int dist = neighbour.second;
-                if (dist+d < result[side]) {
-                    result[side] = dist+d;
-                    pq.push({dist+d, side});
+            for (auto &vec : adj[node]) {
+                int adjNode = vec.first;
+                int d = vec.second;
+                if (dist + d < result[adjNode]) {
+                    result[adjNode] = dist+d;
+                    pq.push({adjNode, d});
                 }
             }
         }
-        int ans = *max_element(result.begin()+1, result.end());
-        return ans == INT_MAX ? -1 : ans;    
+        int ans = 0; 
+        for (int i = 1; i<n+1; i++) {
+            ans = max(ans, result[i]);
+        }
+        return ans == INT_MAX ? -1 : ans;
     }
 };
