e27c42957035aa9426ef3670c0145b64c75b4c7b
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-07-27 22:41:26 -0400

    Time: 280 ms (5.04%), Space: 83.1 MB (5.33%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    typedef pair <int, pair<int, int>> P;
-    vector<vector<int>> directions{{1,0}, {-1,0}, {1,-1}, {0,-1}, {-1,-1}, {1,1}, {0,1}, {-1,1}};
+    typedef pair<int, pair<int, int>> P;
+    vector<vector<int>> directions {{-1,0}, {1,0}, {-1,1}, {0,1}, {1,1}, {-1,-1}, {0,-1}, {1,-1}};
+
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+
         int m = grid.size();
         int n = grid[0].size();
-        
+
         if (m == 0 || n == 0 || grid[0][0] != 0) return -1;
 
         auto isSafe = [&] (int x, int y) {
-            return x >= 0 && x < m && y >= 0 && y < n;
+            return x >= 0 && x < m && y >= 0 && y < n; 
         };
 
         vector<vector<int>> result (m, vector<int> (n, INT_MAX));
-        priority_queue<P, vector<P>, greater<P>> pq;
-        pq.push({0,{0,0}});
         result[0][0] = 0;
+        priority_queue<P, vector<P>, greater<P>> pq;
+        pq.push({0, {0,0}});
 
-        while(!pq.empty()) {
+        while (!pq.empty()) {
             int d = pq.top().first;
-            pair<int, int> node = pq.top().second;
+            auto node = pq.top().second;
             int x = node.first;
             int y = node.second;
             pq.pop();
@@ -28,16 +30,13 @@ public:
                 int x_ = x + dir[0];
                 int y_ = y + dir[1];
 
-                int dist = 1;
-
-                if (isSafe(x_,y_) && !grid[x_][y_] && d+dist < result[x_][y_]) {
-                    pq.push({d+dist, {x_, y_}});
-                    result[x_][y_] = d+dist;
+                if (isSafe(x_, y_) && !grid[x_][y_] && d+1 < result[x_][y_]) {
+                    pq.push({d+1, {x_,y_}});
+                    result[x_][y_] = d+1;
                 }
             }
         }
 
-        if (result[m-1][n-1] == INT_MAX) return -1;
-        return result[m-1][n-1]+1;
+        return result[m-1][n-1] == INT_MAX ? -1 : result[m-1][n-1]+1;
     }
 };
