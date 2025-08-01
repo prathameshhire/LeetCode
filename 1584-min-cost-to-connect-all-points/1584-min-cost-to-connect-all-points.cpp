4641207153114242498b9228455836376470c81c
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-07-31 22:36:00 -0400

    Time: 1667 ms (5.67%), Space: 345.7 MB (5.01%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,51 +1,77 @@
 class Solution {
 public:
-    typedef pair<int, int> P;
-    int minCostConnectPoints(vector<vector<int>>& points) {
-        int V = points.size();
+    vector<int> parent;
+    vector<int> rank;
 
-        vector<vector<P>> adj(V);
-
-        for (int i = 0; i<V; i++) {
-            for (int j = i+1; j<V; j++) {
-                int x1 = points[i][0];
-                int y1 = points[i][1];
-
-                int x2 = points[j][0];
-                int y2 = points[j][1];
-                int d = abs(x1-x2) + abs(y1-y2);
-
-                adj[i].push_back({j,d});
-                adj[j].push_back({i,d});
-            }
+    int find (int x) {
+        if (x == parent[x]) 
+            return x;
+    
+        return parent[x] = find(parent[x]);
+    }
+    
+    void Union (int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+    
+        if (x_parent == y_parent) 
+            return;
+    
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
         }
-
-        return prims(adj, V);
     }
 
-    int prims(vector<vector<P>> adj, int V) {
-        priority_queue<P, vector<P>, greater<P>> pq;
-        pq.push({0,0});
-        vector<bool> inMST(V, false);
+    int kruskal(vector<vector<int>> &vec) {
         int sum = 0;
+        for (auto &temp : vec) {
+            int u = temp[0];
+            int v = temp[1];
+            int wt = temp[2];
 
-        while (!pq.empty()) {
-            int node = pq.top().second;
-            int w = pq.top().first;
-            pq.pop();
+            int parent_u = find(u);
+            int parent_v = find(v);
 
-            if (inMST[node]) continue;
-            inMST[node] = true;
-            sum += w;
-
-            for (auto &tmp : adj[node]) {
-                int v = tmp.first;
-                int wt = tmp.second;
-                if (!inMST[v]) {
-                    pq.push({wt, v});
-                }
+            if (parent_u != parent_v) {
+                Union(parent_u, parent_v);
+                sum+=wt;
             }
         }
         return sum;
+    }
+
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int V = points.size();
+        vector<vector<int>> vec;
+        parent.resize(V);
+        rank.resize(V,0);
+        for (int i = 0; i<V; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i<V; i++) {
+            for (int j = 0; j<V; j++) {
+                int x1 = points[i][0];
+                int y1 = points[i][1];
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+
+                int d = abs(x1-x2) + abs(y1-y2);
+                vec.push_back({i,j,d});
+
+            }
+        }
+        auto comparator = [&] (vector<int> &vec1, vector<int> &vec2) {
+            return vec1[2] < vec2[2];
+        };
+
+        sort(vec.begin(), vec.end(), comparator);
+
+        return kruskal(vec);
     }
 };
