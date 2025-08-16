d29a66b2b4f4cf88fe9a5c734b9a7b622497418a
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-08-16 01:18:23 -0400

    Time: 0 ms (100%), Space: 10.3 MB (77%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int i, int n, vector<int> &t) {
-        if (i > n) return 0;
-        if (t[i] != -1) return t[i];
-        int steal = nums[i] + solve(nums, i+2, n, t);
-        int skip = solve(nums, i+1, n, t);
-        return t[i] = max(steal, skip);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1 ) return nums[0];
+        if (n == 1) return nums[0];
         if (n == 2) return max(nums[0], nums[1]);
-        vector<int> t(n+1, -1);
-        int zero = solve(nums, 0, n-2, t);
-        t = vector<int> (n+1, -1);
-        int one = solve(nums, 1, n-1, t);
-        return max(zero, one);
+        int one = solve(0, n-2, nums);
+        int two = solve(1, n-1, nums);
+        return max(one, two);
+    }
+    int solve (int l, int r, vector<int> &nums) {
+        int prev = 0, prevPrev = 0;
+        for (int i = l; i <= r; i++) {
+            int skip = prev;
+            int take = nums[i] + prevPrev;
+            int temp = max(skip, take);
+            prevPrev = prev;
+            prev = temp;
+        }
+        return prev;
     }
 };
