979d51ffc3953a04998dca448ab9db00a99cfdd4
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-09-17 00:36:17 -0400

    Time: 51 ms (15.67%), Space: 11.8 MB (85.46%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -6,22 +6,20 @@ public:
         m = s1.length();
         n = s2.length();
         memset(t, -1, sizeof(t));
-        return solve(s1, s2, 0, 0);
+        return solve(s1, s2, m, n);
     }
 
-    int solve(string &s1, string &s2, int i, int j) {
-        if(t[i][j] != -1) return t[i][j];
-        
-        if (i == m) return t[i][j] = n-j;
-        else if (j == n) return t[i][j] = m-i;
-        
-        if (s1[i] == s2[j]) return t[i][j] = solve(s1, s2, i+1, j+1);
-        else {
-            int ins = 1 + solve(s1, s2, i, j+1);
-            int del = 1 + solve(s1, s2, i+1, j);
-            int replace = 1 + solve(s1, s2, i+1, j+1);
+    int solve(string &s1, string &s2, int m, int n) {
+        if(t[m][n] != -1) return t[m][n];
 
-            return t[i][j] = min({ins, del, replace});
+        if (m == 0 || n == 0) return m+n;
+        if (s1[m-1] == s2[n-1]) return t[m][n] = solve(s1, s2, m-1, n-1);
+        else {
+            int ins = 1 + solve(s1, s2, m, n-1);
+            int del = 1 + solve(s1, s2, m-1, n);
+            int replace = 1 + solve(s1, s2, m-1, n-1);
+
+            return t[m][n] = min({ins, del, replace});
         }
 
         return -1;
