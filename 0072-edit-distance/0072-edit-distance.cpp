1785c32feabbc8ca0e408365c8364eb5bd05e5cb
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-09-17 00:23:51 -0400

    Time: 51 ms (15.67%), Space: 11.8 MB (85.46%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int m, n;
-    int t[500][500];
+    int t[501][501];
     int minDistance(string s1, string s2) {
         m = s1.length();
         n = s2.length();
@@ -10,8 +10,8 @@ public:
     }
 
     int solve(string &s1, string &s2, int i, int j) {
-        if (i == m) return n-j;
-        else if (j == n) return m-i;
+        if (i == m) return t[i][j] = n-j;
+        else if (j == n) return t[i][j] = m-i;
         if(t[i][j] != -1) return t[i][j];
         if (s1[i] == s2[j]) return t[i][j] = solve(s1, s2, i+1, j+1);
         else {
