921f8fa839669db3fb41d2e730a396307fa4d411
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-09-20 23:06:36 -0400

    Time: 27 ms (37.96%), Space: 12.8 MB (44.63%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    int t[1001][1001];
     int countSubstrings(string s) {
         int n = s.length();
         int count = 0;
+
+        memset(t, -1, sizeof(t));
 
         for (int i = 0; i<n; i++) {
             for (int j = i; j<n; j++) {
@@ -17,7 +20,8 @@ public:
 
     bool check(string &s, int i, int j) {
         if (i>j) return true;
-        if (s[i] == s[j]) return check(s, i+1, j-1);
+        if (t[i][j] != -1) return t[i][j];
+        if (s[i] == s[j]) return t[i][j] = check(s, i+1, j-1);
         return false;
     }
 };
