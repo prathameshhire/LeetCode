4dad73fbde4448cb17d392644838263aa5808bbe
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-09-20 23:33:54 -0400

    Time: 27 ms (37.96%), Space: 11.1 MB (48.94%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int t[1001][1001];
     int countSubstrings(string s) {
         int n = s.length();
+
+        vector<vector<bool>> t (n, vector<bool> (n,0));
+
         int count = 0;
+         
+        for (int L = 1; L<=n; L++) {
+            for (int i = 0; i+L-1<n; i++) {
+                int j = i+L-1;
+                if (i == j) t[i][i] = true;
+                else if (i+1 == j) t[i][j] = (s[i] == s[j]);
+                else if (s[i] == s[j] && t[i+1][j-1]) t[i][j] = true;
+                else t[i][j] = false;
 
-        memset(t, -1, sizeof(t));
-
-        for (int i = 0; i<n; i++) {
-            for (int j = i; j<n; j++) {
-                if (check(s, i, j)) {
-                    count++;
-                }
+                if (t[i][j]) count++;
             }
+
         }
 
         return count;
-    }
-
-    bool check(string &s, int i, int j) {
-        if (i>j) return true;
-        if (t[i][j] != -1) return t[i][j];
-        if (s[i] == s[j]) return t[i][j] = check(s, i+1, j-1);
-        return t[i][j] = false;
     }
 };
