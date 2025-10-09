3275397176f2cd0de971f21c6bc84f33b99a667c
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-10-09 04:14:02 -0400

    Time: 0 ms (100%), Space: 9.3 MB (34.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int t[101][101];
-    int solve(int i, int j, int &m, int &n) {
-        if (t[i][j] != -1) return t[i][j];
-        if (i == m-1 && j == n-1) return 1;
-        if ( i >= m || j >= n) return 0;
-        int right = solve(i, j+1, m, n);
-        int left = solve(i+1, j, m, n);
-        return t[i][j] = left + right;
-    }
-
     int uniquePaths(int m, int n) {
-        memset(t, -1, sizeof(t));
-        return solve(0, 0, m, n);
+        vector<vector<int>> t(m, vector<int> (n));
+        for (int i = 0; i<m; i++) {
+            t[i][0] = 1;
+        }
+        for (int j = 0; j<n; j++) {
+            t[0][j] = 1;
+        }
+        for (int i = 1; i<m; i++) {
+            for (int j = 1; j<n; j++) {
+                t[i][j] = t[i-1][j] + t[i][j-1];
+            }
+        }
+        return t[m-1][n-1];
     }
 };
