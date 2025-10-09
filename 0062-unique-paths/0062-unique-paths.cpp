2d0dbbdeba17b2831c0817d08bc86d8ce4124b70
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-10-09 04:01:36 -0400

    Time: 0 ms (100%), Space: 7.9 MB (91.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,7 +4,7 @@ public:
     int solve(int i, int j, int &m, int &n) {
         if (t[i][j] != -1) return t[i][j];
         if (i == m-1 && j == n-1) return 1;
-        if (i < 0 || j < 0 || i >= m || j >= n) return 0;
+        if ( i >= m || j >= n) return 0;
         int right = solve(i, j+1, m, n);
         int left = solve(i+1, j, m, n);
         return t[i][j] = left + right;
