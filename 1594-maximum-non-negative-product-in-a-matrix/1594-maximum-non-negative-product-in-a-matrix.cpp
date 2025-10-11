f41e0ef6e8b67b6ed67a143f7a84a0217e54e579
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-10-11 04:25:12 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
-    int m, n;
     typedef long long ll;
-
     int mod = 1e9+7;
 
-
-    pair<ll, ll> solve(int i, int j, vector<vector<int>>& grid) {
-        if (i == m-1 && j == n-1) return {grid[i][j], grid[i][j]};
-    
-        ll maxVal = LLONG_MIN;
-        ll minVal = LLONG_MAX;
-
-        if (i+1<m) {
-            auto[downMax, downMin] = solve(i+1, j, grid);
-            maxVal = max({maxVal, grid[i][j] * downMax, grid[i][j] * downMin});
-            minVal = min({minVal, grid[i][j] * downMax, grid[i][j] * downMin});
-        }
-
-        if (j+1<n) {
-            auto[rightMax, rightMin] = solve(i, j+1, grid);
-            maxVal = max({maxVal, grid[i][j] * rightMax, grid[i][j] * rightMin});
-            minVal = min({minVal, grid[i][j] * rightMax, grid[i][j] * rightMin});
-        }
-
-        return {maxVal, minVal};
-
-    }
-
     int maxProductPath(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+        int m = grid.size();    
+        int n = grid[0].size();    
 
-        auto[maxVal, minVal] = (solve(0, 0, grid));   
-        return maxVal < 0 ? -1 : maxVal% mod;
+        vector<vector<pair<ll, ll>>> t(m, vector<pair<ll, ll>>(n));
+
+        t[0][0] = {grid[0][0], grid[0][0]};
+
+        for (int j = 1; j<n; j++) {
+            t[0][j].first = t[0][j-1].first * grid[0][j];
+            t[0][j].second = t[0][j-1].second * grid[0][j];
+        }
+
+        for (int i = 1; i<m; i++) {
+            t[i][0].first = t[i-1][0].first * grid[i][0];
+            t[i][0].second = t[i-1][0].second * grid[i][0];
+        }
+
+        for (int i = 1; i<m; i++) {
+            for (int j = 1; j<n; j++) {
+                int upMax = t[i-1][j].first;
+                int upMin = t[i-1][j].second;
+
+                int leftMax = t[i][j-1].first;
+                int leftMin = t[i][j-1].second;
+            
+                t[i][j].first = max({upMax * grid[i][j], upMin * grid[i][j], leftMax * grid[i][j], leftMin * grid[i][j]});
+                t[i][j].second = min({upMax * grid[i][j], upMin * grid[i][j], leftMax * grid[i][j], leftMin * grid[i][j]});
+            }
+        }
+
+        auto[maxProd, minProd] = t[m-1][n-1];
+
+        return maxProd < 0 ? -1 : maxProd % mod;
+
     }
 };
