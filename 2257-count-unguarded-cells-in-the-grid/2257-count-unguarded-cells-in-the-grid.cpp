f7e85594356161c0940070034fe70e1e09968aa3
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-11-05 18:38:34 -0500

    Time: 54 ms (74.62%), Space: 173.1 MB (74.82%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -1,59 +1,45 @@
 class Solution {
 public:
-    void markCellsAlongGuard(int row , int col , vector<vector<int>>& grid) {
-        for (int i = row - 1; i >= 0; i--) {
-            if (grid[i][col] == 4 || grid[i][col] == 7) break;
-
+    void mark(int row, int col, vector<vector<int>> &grid) {
+        for (int i = row-1; i >= 0; i--) {
+            if (grid[i][col] == 2 || grid[i][col] == 3) break;
             grid[i][col] = 1;
         }
-
-        for (int i = row + 1; i < grid.size(); i++) {
-            if (grid[i][col] == 4 || grid[i][col] == 7) break;
-
+        for (int i = row+1; i < grid.size(); i++) {
+            if (grid[i][col] == 2 || grid[i][col] == 3) break;
             grid[i][col] = 1;
         }
-
-        for (int j = col - 1; j >= 0; j--) {
-            if (grid[row][j] == 4 || grid[row][j] == 7) break;
-
+        for (int j = col-1; j >= 0; j--) {
+            if (grid[row][j] == 2 || grid[row][j] == 3) break;
             grid[row][j] = 1;
         }
-
-        for (int j = col + 1; j < grid[0].size(); j++) {
-            if (grid[row][j] == 4 || grid[row][j] == 7) break;
-
+        for (int j = col+1; j < grid[0].size(); j++) {
+            if (grid[row][j] == 2 || grid[row][j] == 3) break;
             grid[row][j] = 1;
         }
     }
-
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        vector<vector<int>> grid(m , vector<int>(n , 0));
-
-        for (vector<int>& v : guards) {
-            int i = v[0];
-            int j = v[1];
-            grid[i][j] = 7;
+        vector<vector<int>> grid(m, vector<int>(n, 0));
+        for (auto &vec : guards) {
+            int i = vec[0];
+            int j = vec[1];
+            grid[i][j] = 2;
         }
-
-        for (vector<int>& v : walls) {
-            int i = v[0];
-            int j = v[1];
-            grid[i][j] = 4;
+        for (auto &vec : walls) {
+            int i = vec[0];
+            int j = vec[1];
+            grid[i][j] = 3;
         }
-
-        for (vector<int> guard : guards) {
-            int i = guard[0];
-            int j = guard[1];
-
-            markCellsAlongGuard(i , j , grid);
+        for (auto &guard : guards) {
+            mark(guard[0], guard[1], grid);
         }
-
-        int cnt = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) cnt++;
+        int count = 0;
+        for (int i = 0; i<m; i++) {
+            for (int j = 0; j<n; j++) {
+                if (grid[i][j] == 0) count++;
             }
         }
-        return cnt;
+
+        return count;
     }
 };
