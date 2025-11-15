0d2585c231c82d4b5b3d9e5f6f7580e3cb1099a3
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2025-11-14 22:40:19 -0500

    Time: 48 ms (41.12%), Space: 86.5 MB (75.7%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -1,28 +1,28 @@
 class Solution {
- public:
-  vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-    vector<vector<int>> ans(n, vector<int>(n));
-    vector<vector<int>> prefix(n, vector<int>(n + 1));
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> mat(n, vector<int>(n, 0));
+        for (auto& q : queries) {
+            int row1 = q[0];
+            int col1 = q[1];
 
-    for (const vector<int>& query : queries) {
-      const int row1 = query[0];
-      const int col1 = query[1];
-      const int row2 = query[2];
-      const int col2 = query[3];
-      for (int i = row1; i <= row2; ++i) {
-        ++prefix[i][col1];
-        --prefix[i][col2 + 1];
-      }
+            int row2 = q[2];
+            int col2 = q[3];
+
+            for (int i = row1; i <= row2; i++) {
+                mat[i][col1] += 1;
+                if (col2 + 1 < n) {
+                    mat[i][col2 + 1] -= 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                mat[i][j] += mat[i][j - 1];
+            }
+        }
+
+        return mat;
     }
-
-    for (int i = 0; i < n; ++i) {
-      int sum = 0;
-      for (int j = 0; j < n; ++j) {
-        sum += prefix[i][j];
-        ans[i][j] = sum;
-      }
-    }
-
-    return ans;
-  }
 };
