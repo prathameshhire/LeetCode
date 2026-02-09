f0681537dbda30d83b125000bb49f263098aea04
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-02-10 05:12:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    vector<int> quad;
-    
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        if (nums.size() < 4) return {};
-        sort(nums.begin(), nums.end());    
-        kSum(nums, 4, 0, (long long) target);
-        return res;
-    }
-private:
-    void kSum (vector<int> &nums, int k, int start, long long target) {
-        if (k==2) {
-            int l = start, r = nums.size()-1;
-            while (l<r) {
-                long long sum = (long long) nums[l] + nums[r];
-                if (sum < target) l++;
-                else if (sum > target) r--;
-                else {
-                    quad.push_back(nums[l]);
-                    quad.push_back(nums[r]);
-                    res.push_back(quad);
-                    quad.pop_back();
-                    quad.pop_back();
-                    l++;
-                    r--;
-                    while (l<r && nums[l] == nums[l-1]) l++;
+        int n = nums.size();
+        set<vector<int>> st;
+        for (int i = 0; i<n; i++) {
+            for (int j = i+1; j<n; j++) {
+                for (int k = j+1; k<n; k++) {
+                    for (int l = k+1; l<n; l++) {
+                        long long sum = nums[i] + nums[j];
+                        sum += nums[k] + nums[l];
+                        if (sum == target) {
+                            vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                            sort(temp.begin(), temp.end());
+                            st.insert(temp);
+                        }
+                    }
                 }
             }
-            return;
         }
-
-        for (int i = start; i<nums.size()-k+1; i++) {
-            while (i > start && nums[i] == nums[i-1] && i< nums.size()-k+1) i++;
-            quad.push_back(nums[i]);
-            kSum(nums, k-1, i+1, target - nums[i]);
-            quad.pop_back();
-        }
+        vector<vector<int>> res (st.begin(), st.end());
+        return res;
     }
 };
-
