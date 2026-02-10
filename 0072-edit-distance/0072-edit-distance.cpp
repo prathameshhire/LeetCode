e4f526e09f38ae783cbc54d3460ba07070c44f59
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-02-10 06:33:23 +0530

    Time: 7 ms (62.22%), Space: 13.2 MB (41.99%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
+    int minDistance(string s1, string s2) {
+        int m = s1.length();
+        int n = s2.length();
 
-    int t[501][501];
-    int m,n;
+        vector<vector<int>> t(m+1, vector<int> (n+1));
 
-    int solve(string &s1, string &s2, int m, int n) {
-        if (t[m][n] != -1) return t[m][n];
-        if (m == 0 || n == 0) return m+n;
-
-        if (s1[m-1] == s2[n-1]) return t[m][n] = solve(s1, s2, m-1, n-1);
-
-        else {
-            int insertC = 1+(solve(s1, s2, m, n-1));
-            int deleteC = 1+(solve(s1, s2, m-1, n));
-            int replaceC = 1+(solve(s1, s2, m-1, n-1));
-            return t[m][n] = min({insertC, deleteC, replaceC});
+        for (int i = 0; i<=m; i++) {
+            for (int j = 0; j<=n; j++) {
+                if (i == 0 || j == 0) {
+                    t[i][j] = i+j;
+                }
+                else if (s1[i-1] == s2[j-1]) t[i][j] = t[i-1][j-1];
+                else {
+                    t[i][j] = 1 + min({t[i][j-1], t[i-1][j], t[i-1][j-1]});
+                }
+            }
         }
 
-        return -1;
-    }
-
-    int minDistance(string s1, string s2) {
-        memset(t, -1, sizeof(t));
-        m = s1.length();
-        n = s2.length();
-
-        return solve(s1, s2, m, n);
+        return t[m][n];
     }
 };
