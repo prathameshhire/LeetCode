357a5f7b13f6024c32adf9b646f416bdb2acf805
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-02-11 03:27:53 +0530

    Time: 26 ms (44.69%), Space: 43.1 MB (45.97%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> count(26, 0);
-        for (char task : tasks) {
-            count[task - 'A']++;
+        vector<int> mp(26,0);
+        for (char c : tasks) {
+            mp[c-'A']++;
         }
-        
+
         priority_queue<int> maxHeap;
-        for (int cnt : count) {
-            if (cnt > 0) {
-                maxHeap.push(cnt);
+        for (int i = 0; i<26; i++) {
+            if (mp[i] > 0) {
+                maxHeap.push(mp[i]);
             }
         }
-        
-        int time = 0;
+
         queue<pair<int, int>> q;
+
+        int time = 0;
         while (!maxHeap.empty() || !q.empty()) {
             time++;
-            
             if (maxHeap.empty()) {
                 time = q.front().second;
-            } else {
-                int cnt = maxHeap.top() - 1;
+            }
+            else {
+                int freq = maxHeap.top()-1;
                 maxHeap.pop();
-                if (cnt > 0) {
-                    q.push({cnt, time + n});
+                if (freq > 0) {
+                    q.push({freq, time+n});
                 }
             }
-            
-            if (!q.empty() && q.front().second == time) {
-                maxHeap.push(q.front().first);
-                q.pop();
+            if (!q.empty()) {
+                if (time == q.front().second) {
+                    maxHeap.push(q.front().first);
+                    q.pop(); 
+                }
             }
         }
-        
+
         return time;
+
     }
 };
