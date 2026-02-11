6a79a969fa6177e6e0103b366c396da634eb6eac
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-02-11 11:51:29 +0530

    Time: 0 ms (100%), Space: 8.6 MB (25.18%) - LeetHub

---
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int distributeCandies(int n, int limit) {
+        int ways = 0;
+        for (int i = 0; i<=limit; i++) {
+            for (int j = 0; j<= limit; j++) {
+                int k = n - i - j;
+                if (k>=0 && k <= limit) ways++;
+            }
+        }
+        return ways;
+    }
+};
