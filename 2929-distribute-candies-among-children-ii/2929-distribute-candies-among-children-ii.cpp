1ade4785167ba3edb0e7e2ed82504e64fa9d186d
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-02-13 09:23:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
+++ 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     long long distributeCandies(int n, int limit) {
+        return dfs(0, n, limit);
+    }
+
+private:
+    long long dfs(int child, int left, int limit) {
+        if (left < 0)
+            return 0;
+        if (child == 3)  
+            return left == 0;  
+
         long long ways = 0;
-
-        int minCh1 = max(0, n - 2*limit);
-        int maxCh1 = min(n, limit);
-
-        for(int i = minCh1; i <= maxCh1; i++) { 
-
-            int N = n-i; 
-
-            int minCh2 = max(0, N - limit);
-            int maxCh2 = min(N, limit);
-
-            ways += maxCh2 - minCh2 + 1;
-        }
+        
+        for (int give = 0; give <= min(limit, left); ++give)
+            ways += dfs(child + 1, left - give, limit);
 
         return ways;
     }
