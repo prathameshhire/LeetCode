95da46231dcf551e81df39224b4230a9c41acad5
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-02-17 05:15:26 +0530

    Time: 0 ms (100%), Space: 23.6 MB (43.43%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.cpp
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -1,57 +1,60 @@
 class MyCircularQueue {
+private:
+    struct ListNode {
+        int val;
+        ListNode* next;
+        ListNode* prev;
+        ListNode(int v, ListNode* n = nullptr, ListNode* p = nullptr) {
+            val = v;
+            next = n;
+            prev = p;
+        }
+    };
+
+    int space;
+    ListNode* left;
+    ListNode* right;
+
 public:
-    vector<int> q;
-    int capacity;
-    int size;
-    int rear;
-    int front;
     MyCircularQueue(int k) {
-        q = vector<int> (k);
-        capacity = k;
-        rear = -1;
-        front = 0;
-        size = 0;
+        space = k;
+        left = new ListNode(0);
+        right = new ListNode(0, nullptr, left);
+        left->next = right;
     }
-    
+
     bool enQueue(int value) {
         if (isFull()) return false;
-        rear = (rear+1)%capacity;
-        q[rear] = value;
-        size++;
+        ListNode* cur = new ListNode(value, right, right->prev);
+        right->prev->next = cur;
+        right->prev = cur;
+        space--;
         return true;
     }
-    
+
     bool deQueue() {
         if (isEmpty()) return false;
-        front = (front+1)%capacity;
-        size--;
+        ListNode* tmp = left->next;
+        left->next = left->next->next;
+        left->next->prev = left;
+        delete tmp;
+        space++;
         return true;
     }
-    
+
     int Front() {
-        return isEmpty() ? -1 : q[front];
+        return isEmpty() ? -1 : left->next->val;
     }
-    
+
     int Rear() {
-        return isEmpty() ? -1 : q[rear];
+        return isEmpty() ? -1 : right->prev->val;
     }
-    
+
     bool isEmpty() {
-        return size == 0;
+        return left->next == right;
     }
-    
+
     bool isFull() {
-        return size == capacity;
+        return space == 0;
     }
 };
-
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue* obj = new MyCircularQueue(k);
- * bool param_1 = obj->enQueue(value);
- * bool param_2 = obj->deQueue();
- * int param_3 = obj->Front();
- * int param_4 = obj->Rear();
- * bool param_5 = obj->isEmpty();
- * bool param_6 = obj->isFull();
- */
