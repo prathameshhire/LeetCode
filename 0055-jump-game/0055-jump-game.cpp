4f6a9b4530f4a81d5fc0d5a0f596c5507be46a2f
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-02-19 12:13:10 +0530

    Time: 39 ms (17.7%), Space: 53.2 MB (14.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int t [10001];
-    bool solve(vector<int> &nums, int n, int index) {    
-        if (index == n-1) return true;
-        if (t[index] != -1) return t[index]; 
-        for (int i = 1; i<=nums[index]; i++) {
-            if (solve(nums, n, index+i)) return t[index] = true;
-        }
-        return t[index] = false;
-    }
     bool canJump(vector<int>& nums) {
-        memset(t, -1, sizeof(t));
         int n = nums.size();
-        return solve(nums, n, 0);
+        vector<bool> t(n, false);
+
+        t[0] = true;
+        
+        for(int i = 1; i<n; i++) {
+            for (int j = i-1; j>=0; j--) {
+                if (t[j] == true && j+nums[j] >= i) {
+                    t[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return t[n-1];
     }
 };
