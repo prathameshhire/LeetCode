de5bb3fdabc9b3c00b02e37a324ab2fa24a373c2
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-03-03 05:21:59 -0500

    Time: 79 ms (34.38%), Space: 75.4 MB (27.68%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     
+    unordered_map<string, int> mp;
+
     bool hasDuplicate(string &s1, string &s2) {
         vector<int> vec(26,0);
 
@@ -19,6 +21,10 @@ public:
     int solve(string temp, int i, vector<string> & arr, int &n) {
         if (i == n) return temp.length();
 
+        if (mp.find(temp) != mp.end()) {
+            return mp[temp];
+        }
+
         int include = 0;
         int exclude = 0;
 
@@ -30,12 +36,13 @@ public:
             exclude = solve(temp, i+1, arr, n);
         }
 
-        return max(include, exclude);
+        return mp[temp] = max(include, exclude);
     }
 
     int maxLength(vector<string>& arr) {
         string temp = "";
         int n = arr.size();
+        // mp.clear();
         return solve(temp, 0, arr, n);
     }
 
