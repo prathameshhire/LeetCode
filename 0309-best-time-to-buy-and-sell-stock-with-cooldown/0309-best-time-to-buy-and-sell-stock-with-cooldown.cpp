58a1608d85d4db29121f968e81d68fcf6130611a
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-03-17 00:26:41 -0400

    Time: 11 ms (6.59%), Space: 15.1 MB (85.27%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int n;
-    int t[5001][2];
-    int solve(vector<int> &prices, int day, bool buy) {
-        if (day >= n) return 0;
-        if (t[day][buy] != -1) return t[day][buy];
-        int profit = 0;
-        if (buy) {
-            int take = solve(prices, day+1, false) - prices[day];
-            int not_take = solve(prices, day+1, true);
-            profit = max({profit, take, not_take});
-        }
-        else {
-            int sell = solve(prices, day+2, true) + prices[day];
-            int not_sell = solve(prices, day+1, false);
-            profit = max({profit, sell, not_sell});
-        }
-        return t[day][buy] = profit;
-    }
     int maxProfit(vector<int>& prices) {
-        memset(t, -1, sizeof(t));
-        n = prices.size();
-        return solve(prices, 0, true);
+        int n = prices.size();
+        if (n == 0 || n == 1) return 0;
+        vector<int> t(n,0);
+        t[0] = 0;
+        t[1] = max(prices[1] - prices[0], 0);
+
+        for (int i = 2; i<n; i++){
+            t[i] = t[i-1];
+            for (int j = 0; j<=i-1; j++) {
+                int today_profit = prices[i]-prices[j];
+                int prev_profit = j >= 2 ? t[j-2] : 0;
+                t[i] = max(t[i], today_profit+prev_profit);
+            }
+        }
+
+        return t[n-1];
     }
 };
