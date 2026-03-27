b15f7e896627923e04c65d382b0700a521ae7e50
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-03-26 23:18:23 -0400

    Time: 74 ms (9.6%), Space: 182.4 MB (11.89%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,12 +1,46 @@
 class Solution {
 public:
-    int minimizeArrayValue(vector<int>& nums) {
-        long long ans = nums[0];    
-        long long total = nums[0];    
-        for(int i = 1; i < nums.size(); i++){
-            total += nums[i];
-            ans = max(ans, (total + i)/(i+1));
+    
+    bool isValid(vector<int> &nums, int mid_max, int n) {
+        
+        vector<long long> arr(begin(nums), end(nums));
+        
+        for(int i = 0; i<n-1; i++) {
+            
+            if(arr[i] > mid_max)
+                return false;
+            
+            long long buffer = mid_max - arr[i];
+            
+            arr[i+1] = arr[i+1] - buffer;   
+            
         }
-        return (int)ans;
+        
+        return arr[n-1] <= mid_max;
+        
+    }
+    
+    int minimizeArrayValue(vector<int>& nums) {
+        int n = nums.size();
+        
+        int maxL = 0;
+        int maxR = *max_element(begin(nums), end(nums));
+        
+        int result;
+        
+        while(maxL <= maxR) {
+            
+            int mid_max = maxL + (maxR-maxL)/2;
+            
+            if(isValid(nums, mid_max, n)) {
+                result = mid_max;
+                maxR   = mid_max-1;
+            } else {
+                maxL = mid_max+1;
+            }
+            
+        }
+        
+        return result;
     }
 };
