60ae6a1a88a55000f8ad81b617b06b82f0efe294
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-03-29 00:43:59 -0400

    Time: 15 ms (100%), Space: 86.7 MB (51.94%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    int n;
-    bool valid(vector<int> &nums, int mid, int &p) {
-        int i = 0;
-        int count = 0;
-        while(i<n-1) {
-            if (nums[i+1]-nums[i] <= mid) {
-                count++;
-                i+=2;
-            }
-            else {
+    bool valid(vector<int> &nums, int mid, int p) {
+        int correct = 0;
+        for(int i = 0; i<nums.size()-1;i++) {
+            if (nums[i+1] - nums[i] <= mid) {
+                correct++;
                 i++;
-            }
+            } else continue;
         }
-
-        return count>=p;
+        return correct >= p;
     }
     int minimizeMax(vector<int>& nums, int p) {
-        n = nums.size();
+        int n = nums.size();
         sort(nums.begin(), nums.end());
         int l = 0;
         int r = nums[n-1] - nums[0];
         int result = INT_MAX;
-        while (l<=r) {
-            int mid = l+(r-l)/2;
-            if(valid(nums,mid,p)) {
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+            if (valid(nums, mid, p)) {
                 result = mid;
                 r = mid-1;
             }
-            else {
-                l = mid+1;
-            }
+            else l = mid+1;
         }
         return result;
     }
