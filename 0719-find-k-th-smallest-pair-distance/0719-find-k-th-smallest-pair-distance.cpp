4488a4aef3a5e30891446692e87213bf3e7f2b40
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-04-02 23:15:44 -0400

    Time: 0 ms (100%), Space: 14.1 MB (13.64%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
+    int possible(vector<int> &nums, int mid) {
+        int i = 0;
+        int j = 1;
+        int n = nums.size();
+        int count = 0;
+        while (j<n) {
+            while(nums[j] - nums[i] > mid){
+                i++;
+            }
+            count += (j-i);
+            j++;
+        }
+        return count;
+    }
     int smallestDistancePair(vector<int>& nums, int k) {
         int n = nums.size();
-        int maxSize = *max_element(nums.begin(), nums.end());
-        vector<int> vec (maxSize+1,0);
-        for (int i = 0; i<n; i++) {
-            for (int j = i+1; j<n; j++) {
-                int val = abs(nums[i] - nums[j]);
-                vec[val]++;
-            }
-        }
-        for (int i = 0; i<vec.size(); i++) {
-            k -= vec[i];
-            if (k <= 0) return i;
-        }
-        return -1;
+        sort(nums.begin(), nums.end());
+        int l = 0;
+        int r = nums[n-1] - nums[0];
+        int result = 0;
+        while (l<=r) {
+            int mid = l+(r-l)/2;
+            int countPair = possible(nums, mid);
+            if (countPair >= k) {
+                r = mid-1;
+                result = mid;
+            } 
+            else l = mid+1;
+        }        
+        return result;
     }
 };
