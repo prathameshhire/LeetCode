85a2b18b85adc466568ec1561c5a02d1363e7ca4
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-04-04 13:47:45 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3449-maximize-the-minimum-game-score/3449-maximize-the-minimum-game-score.cpp
+++ 3449-maximize-the-minimum-game-score/3449-maximize-the-minimum-game-score.cpp
@@ -1,61 +1,40 @@
 class Solution {
 public:
-    typedef long long ll;
-
-    bool isPossible(ll target, int m, vector<int>& points, int n) {
-        ll moves = 0;
-
-        ll advanceGamesToNext = 0;
-        ll normalMove = 0;
-
-        for(int i = 0; i < n && moves <= m; i++) {
+    bool possible(vector<int> &points, int m, long long target) {
+        long long moves = 0;
+        long long advancedMoves = 0;
+        long long normalMoves = 0;
+        for (int i = 0; i<points.size() && moves <= m; i++) {
             int gamePoint = points[i];
-
-            ll games = (target + gamePoint -1) / gamePoint; //ceil(target/gamePoint);
-            //cile(a/b) = (a + b - 1) / b
-
-            if(advanceGamesToNext >= games) {
-                advanceGamesToNext = 0;
-                normalMove += 1;
-            } else {
-                ll pointsAlreadyCovered = advanceGamesToNext * gamePoint;
-
-                games = (target - pointsAlreadyCovered + gamePoint - 1) / gamePoint; 
-                //ceil((target - pointsAlreadyCovered) / gamePoint);
-
-                moves += (2*games) - 1;
-
-                advanceGamesToNext = max(games-1, 0LL);
-
-                moves += normalMove;
-
-                normalMove = 0;
+            long long games = (target + gamePoint - 1)/gamePoint;
+            if(advancedMoves >= games) {
+                advancedMoves = 0;
+                normalMoves+=1;
+            }
+            else {
+                long long pointsCovered = advancedMoves * gamePoint;
+                games = (target - pointsCovered + gamePoint - 1)/gamePoint;
+                moves += (2*games)-1;
+                advancedMoves = max(games-1,0LL);
+                moves += normalMoves;
+                normalMoves = 0;
             }
         }
-        
         return moves <= m;
     }
-    
+
     long long maxScore(vector<int>& points, int m) {
-        int n = points.size();
-
-        ll l = 1;
-        ll r = 1e15;//10^15;
-        //10^9 * 10^6 = 10^15
-
-        ll result = 0;
-
-        while(l <= r) {
-            ll mid = l + (r-l)/2; //target score
-
-            if(isPossible(mid, m, points, n) == true) {
+        long long l = 1;
+        long long r = 10^15;
+        long long result = 0;
+        while (l <= r) {
+            long long mid = l+(r-l)/2;
+            if (possible(points, m, mid)) {
                 result = mid;
                 l = mid+1;
-            } else {
-                r = mid-1;
             }
+            else r = mid-1;
         }
-
         return result;
     }
 };
