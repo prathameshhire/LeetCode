bf44ce8b186ab465adc670d2f0133a653d9ce123
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-04-05 12:10:07 -0400

    Time: 54 ms (47.38%), Space: 59.2 MB (98.56%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     typedef long long ll;
-
-    bool isPossible(vector<int>& ranks, ll mid, int cars) {
-        ll carsFixed = 0;
-
-        for(int i = 0; i < ranks.size(); i++) {
-            carsFixed += sqrt(mid/ranks[i]); //ignoring this
+    bool possible(vector<int> &ranks, int cars, ll time) {
+        ll totalCars = 0;
+        for (int i = 0; i<ranks.size(); i++) {
+            totalCars += sqrt(time/ranks[i]); 
         }
-
-        return carsFixed >= cars;
-
+        return totalCars >= cars;
     }
-
     long long repairCars(vector<int>& ranks, int cars) {
         int n = ranks.size();
-
         ll l = 1;
-        int maxR = *max_element(begin(ranks), end(ranks));
+        int maxR = *max_element(ranks.begin(), ranks.end());
         ll r = 1LL * maxR * cars * cars;
-
-        ll result = -1;
-        //T.C: O(n * log(maxR*cars*cars));
-        while(l <= r) { //log(maxR * cars * cars)
+        ll result = 0;
+        while (l <= r) {
             ll mid = l + (r-l)/2;
-
-            if(isPossible(ranks, mid, cars)) { //O(n)
+            if (possible(ranks, cars, mid)) {
                 result = mid;
                 r = mid-1;
-            } else {
-                l = mid+1;
             }
+            else l = mid+1;
         }
-
         return result;
     }
 };
