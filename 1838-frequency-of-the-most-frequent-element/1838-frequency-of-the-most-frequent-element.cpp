77e38f4140a3c16f75970b58f4b8a457e5aaf33b
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-04-19 10:40:19 -0400

    Time: 51 ms (17.26%), Space: 106 MB (32.62%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    int bsearch(int target_idx, int k, vector<int> &nums, vector<long> &prefixSum) {
-        int l = 0;
-        int r = target_idx;
-        int ans = 0;
-        while(l<=r) {
-            int mid = l+(r-l)/2;
-            long count = target_idx - mid + 1;
-            long cumSum = prefixSum[target_idx] - prefixSum[mid] + nums[mid];
-            long targetSum = count * nums[target_idx];
-            if(targetSum - cumSum <= k) {
-                ans = mid;
-                r = mid-1;
-            }
-            else {
-                l = mid+1;
-            }
-        }
-        return target_idx - ans +1;
-    }
     int maxFrequency(vector<int>& nums, int k) {
-        int n = nums.size();
         sort(nums.begin(), nums.end());
-        vector<long> prefixSum(n);
-        prefixSum[0] = nums[0];
-        for(int i = 1; i<n; i++) {
-            prefixSum[i] = prefixSum[i-1] + nums[i];
-        }
+        int n = nums.size();
         int result = 0;
-        for(int i = 0; i<n; i++) {
-            result = max(result, bsearch(i, k, nums, prefixSum));
+        int l = 0;
+        long currSum = 0;
+        for(int r = 0; r<n; r++) {
+            long target = nums[r];
+            currSum += nums[r];
+            while((r-l+1)*target - currSum > k) {
+                currSum -= nums[l];
+                l++;
+            }
+            result = max(result, r-l+1);
         }
         return result;
     }
