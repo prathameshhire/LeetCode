83d1cfa10426983c2cfda4f8f27037d7447eeffa
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-04-21 12:13:45 -0400

    Time: 1 ms (20.19%), Space: 14.1 MB (54.93%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
+        stack<int> st; 
         int n = prices.size();
-        vector<int> result = prices;
-        for(int i = 0; i<n; i++) {
-            for(int j = i+1; j<n; j++) {
-                if(prices[i] >= prices[j]) {
-                    result[i] = prices[i]-prices[j];
-                    break;
-                }
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && prices[i] <= prices[st.top()]) {
+                int idx = st.top();
+                st.pop();
+                prices[idx] -= prices[i];
             }
+            st.push(i);
         }
-        return result;
+
+        return prices;
     }
 };
