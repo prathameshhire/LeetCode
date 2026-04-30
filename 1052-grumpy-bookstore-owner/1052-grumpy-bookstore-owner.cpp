95ff389c43238adeea26cba6cc42a9c7936a0d52
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-04-30 17:59:06 -0400

    Time: 0 ms (100%), Space: 35.8 MB (39.33%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int i = 0, j = 0, n = grumpy.size(), result = 0, answer = 0;
+        for(int k = 0; k<n; k++) {
+            if(!grumpy[k]) result += customers[k];
+        }
+        while (j<n) {
+            (grumpy[j] == 1) ? result += customers[j] : result += 0;
+            answer = max(answer, result);
+            if(j-i+1 == minutes) {
+                if(grumpy[i]) result -= customers[i];
+                i++; 
+            }
+            j++;
+        }
+        return answer;
+    }
+};
