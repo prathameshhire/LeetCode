aa0bb1a9724a57856c7c6f07d173678c13f10d27
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-05-02 14:05:32 -0400

    Time: 10 ms (9.24%), Space: 99.6 MB (79.52%) - LeetHub

---
+++ 0396-rotate-function/0396-rotate-function.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n  = nums.size();
+        int sum = 0;
+        int F = 0;
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+            F += i*nums[i];
+        }
+        int result = F;
+        for(int i = 0; i<=n-1; i++) {
+            int newF = F + sum - n*nums[n-1-i];
+            result = max(result, newF);
+            F = newF;
+        }
+        return result;
+    }
+};
