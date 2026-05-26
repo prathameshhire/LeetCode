a64ab1d04e4a15cc4bb0ab96ddc5b64e719500fb
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-05-25 21:19:15 -0400

    Time: 1 ms (39.91%), Space: 9.5 MB (35.03%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     int numberOfSpecialChars(string word) {
         unordered_set<char> st;
-
         for(char c : word) {
             st.insert(c);
         }
-
         int count = 0;
-
         for(char ch = 'a'; ch <= 'z'; ch++) {
             if(st.count(ch) && st.count(toupper(ch))) {
                 count++;
             }
         }
-
         return count;
     }
 };
