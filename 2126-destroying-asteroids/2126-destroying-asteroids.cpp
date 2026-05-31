4476f3337a187332f6b8be20ffd7720cb407ac8d
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-05-31 15:30:27 -0400

    Time: 36 ms (37.08%), Space: 106.7 MB (81.61%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        sort(asteroids.begin(), asteroids.end());
-        long long m = mass;
-        for(int asteroid: asteroids) {
-            if(asteroid <= m) m += asteroid;
-            else return false;
+        sort(begin(asteroids), end(asteroids));
+
+        long long totalMass = mass;
+
+        for(int &asteroid : asteroids) {
+            if(totalMass < asteroid)
+                return false;
+
+            totalMass += asteroid;
         }
+
         return true;
     }
 };
