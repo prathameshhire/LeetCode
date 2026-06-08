50d5cfb999761a1bf30cbf9a99f39c3ef0886a6f
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-06-07 22:04:46 -0400

    Time: 191 ms (61.62%), Space: 279 MB (84.73%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -1,52 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        // Sets to track unique children and parents
-        unordered_set<int> children, parents;
-        // Map to store parent to children relationships
-        unordered_map<int, vector<pair<int, int>>> parentToChildren;
-
-        // Build graph from parent to child, and add nodes to HashSets
-        for (auto& d : descriptions) {
-            int parent = d[0], child = d[1], isLeft = d[2];
-            parents.insert(parent);
-            parents.insert(child);
-            children.insert(child);
-            parentToChildren[parent].emplace_back(child, isLeft);
+        unordered_map<int, TreeNode*> mp;
+        unordered_set<int> childSet;
+        for(vector<int> &vec : descriptions) {
+            int parent = vec[0];
+            int child = vec[1];
+            int isLeft = vec[2];
+            
+            if(mp.find(parent) == mp.end()) mp[parent] = new TreeNode(parent);
+            if(mp.find(child) == mp.end()) mp[child] = new TreeNode(child);
+            
+            if(isLeft) mp[parent]->left = mp[child];
+            else mp[parent]->right = mp[child];
+            childSet.insert(child);
         }
 
-        // Find the root node by checking which node is in parents but not in
-        // children
-        for (auto it = parents.begin(); it != parents.end();) {
-            if (children.find(*it) != children.end()) {
-                it = parents.erase(it);
-            } else {
-                ++it;
-            }
-        }
-        TreeNode* root = new TreeNode(*parents.begin());
-
-        // Starting from root, use BFS to construct binary tree
-        queue<TreeNode*> queue;
-        queue.push(root);
-
-        while (!queue.empty()) {
-            TreeNode* parent = queue.front();
-            queue.pop();
-            // Iterate over children of current parent
-            for (auto& childInfo : parentToChildren[parent->val]) {
-                int childValue = childInfo.first, isLeft = childInfo.second;
-                TreeNode* child = new TreeNode(childValue);
-                queue.push(child);
-                // Attach child node to its parent based on isLeft flag
-                if (isLeft == 1) {
-                    parent->left = child;
-                } else {
-                    parent->right = child;
-                }
-            }
+        for(vector<int> &vec : descriptions) {
+            int parent = vec[0];
+            if(!childSet.count(parent)) return mp[parent];
         }
 
-        return root;
+        return NULL;
     }
 };
