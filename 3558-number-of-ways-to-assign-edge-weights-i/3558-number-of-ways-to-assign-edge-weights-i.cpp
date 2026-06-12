c18bb0f65370752585a37cea71ae09ff5e4a0e63
Prathamesh Hire <114651839+prathameshhire@users.noreply.github.com>
2026-06-12 00:36:00 -0400

    Time: 483 ms (19.74%), Space: 395.4 MB (14.47%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -1,37 +1,34 @@
 class Solution {
-    static constexpr int mod = 1e9 + 7;
-    int qpow(int x, int y) {
-        int res = 1;
-        for (; y; y >>= 1) {
-            if (y & 1) {
-                res = 1ll * res * x % mod;
-            }
-            x = 1ll * x * x % mod;
-        }
-        return res;
-    }
-    int dfs(vector<vector<int>>& g, int x, int f) {
-        int max_dep = 0;
-        for (auto& y : g[x]) {
-            if (y == f) {
-                continue;
-            }
-            max_dep = max(max_dep, dfs(g, y, x) + 1);
-        }
-        return max_dep;
+public:
+
+    int M = 1e9 + 7;
+    typedef long long ll;
+    ll power (ll base, ll exponent) {
+        if (exponent == 0) return 1;
+        ll half = power(base, exponent/2);
+        ll result =  (half * half) % M;
+        if(exponent % 2 == 1) result = (result * base) % M; 
+        return result;
     }
 
-public:
-    int assignEdgeWeights(vector<vector<int>>& edges) {
-        int n = edges.size() + 1;
-        vector<vector<int>> g(n + 1);
-        for (auto& e : edges) {
-            int u = e[0];
-            int v = e[1];
-            g[u].emplace_back(v);
-            g[v].emplace_back(u);
+    int getMaxDepth(unordered_map<int, vector<int>> &adj, int root, int parent) {
+        int depth = 0;
+        for(int &ngbr : adj[root]) {
+            if(ngbr == parent) continue;
+            depth = max(depth, getMaxDepth(adj, ngbr, root) + 1);
         }
-        int max_dep = dfs(g, 1, 0);
-        return qpow(2, max_dep - 1);
+        return depth;
+    }
+
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        for(auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        int maxDepth = getMaxDepth(adj, 1, -1); 
+        return power(2, maxDepth-1); 
     }
 };
